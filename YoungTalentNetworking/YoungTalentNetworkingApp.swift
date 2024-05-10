8eb1b82b2400b3a854a4ff520f3b0a7a43fc8b35
Eser Kucuker <eser.kucuker@mobven.com>
2024-05-10 11:26:38 +0300

    first init

---
+++ YoungTalentNetworking/YoungTalentNetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoungTalentNetworkingApp.swift
+//  YoungTalentNetworking
+//
+//  Created by Eser Kucuker on 10.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct YoungTalentNetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
