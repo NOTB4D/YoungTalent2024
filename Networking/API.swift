7c810e4d2c8542ddc373d72f02b6bdac18ebeec4
Eser Kucuker <eser.kucuker@mobven.com>
2024-05-10 13:46:03 +0300

    add api enun

---
+++ Networking/API.swift
@@ -0,0 +1,30 @@
+//
+//  API.swift
+//  YoungTalentNetworking
+//
+//  Created by Eser Kucuker on 10.05.2024.
+//
+
+import Foundation
+
+public enum API {
+    static func prepareUrl(withPath path: String) -> URL {
+        guard let baseURL = Bundle.main.infoForKey("BASE_URL") else {
+            fatalError("Could not create BaseURL")
+
+        }
+
+        guard let url = URL(string: "\(baseURL)\(path)") else {
+            fatalError("Could not create url")
+        }
+
+        return url
+    }
+
+    static func getHeader(contentType: ContentType) -> [String: String] {
+        var headers: [String: String] = [:]
+        //header["Authorizaition"] = "Bearer "
+        headers["Content-Type"] = contentType.rawValue
+        return headers
+    }
+}
