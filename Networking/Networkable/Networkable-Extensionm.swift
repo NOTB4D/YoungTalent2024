3d98b62c1f00374569363d0338ba2839e4fd2504
Eser Kucuker <eser.kucuker@mobven.com>
2024-05-10 13:47:34 +0300

    add getRequest func

---
+++ Networking/Networkable/Networkable-Extensionm.swift
@@ -0,0 +1,54 @@
+//
+//  Networkable-Extensionm.swift
+//  YoungTalentNetworking
+//
+//  Created by Eser Kucuker on 10.05.2024.
+//
+
+import Foundation
+
+
+extension Networkable {
+    func getRequest(
+        queryItem: [String: String] = [:],
+        path: String,
+        httpMethod: RequestMethod = .get
+    ) async -> URLRequest {
+        var url = API.prepareUrl(withPath: path)
+        url.append(queryItems: [.init(name: "api_key", value: "a9682a1bcb1c6843474a121144fd9071")])
+        var request = await prepareRequest(
+            url: url,
+            method: httpMethod,
+            contentType: ContentType.json
+        )
+        return request
+    }
+
+    func getRequest<T: Decodable>(
+        data: T,
+        path: String,
+        httpMethod: RequestMethod = .post
+    ) async -> URLRequest {
+        var url = API.prepareUrl(withPath: path)
+        url.append(queryItems: [.init(name: "api_key", value: "a9682a1bcb1c6843474a121144fd9071")])
+        // TODO: Add decodable
+        var request = await prepareRequest(
+            url: url,
+            method: httpMethod,
+            contentType: ContentType.json
+        )
+        return request
+    }
+    ///
+
+    private func prepareRequest(
+        url: URL,
+        method: RequestMethod,
+        contentType: ContentType
+    ) async -> URLRequest {
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        request.allHTTPHeaderFields = API.getHeader(contentType: contentType)
+        return request
+    }
+}
