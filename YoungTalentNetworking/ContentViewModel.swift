d1cdf77f7360d4c9d5529507542005304938511a
Hasan Güler <hasan.guler@mobven.com>
2024-05-24 11:08:12 +0300

    set response data on main thread

--- YoungTalentNetworking/ContentViewModel.swift
+++ YoungTalentNetworking/ContentViewModel.swift
@@ -10,22 +10,25 @@ import Foundation
 extension ContentView {
     final class ViewModel: ObservableObject {
         @Published var movies: [MovieResponse] = []
-
+        
         func getMovies() async {
             let result = await API.Movie.getMovie.fetch(requestModel: [MovieResponse].self)
-            let upComingResponse = await API.Movie.upComing.fetch(requestModel: [MovieResponse].self)
-
+            
             switch result {
             case let .success(response):
-                movies = response
+                await MainActor.run {
+                    movies = response
+                }
             case let .failure(error):
                 print(error.localizedDescription)
             }
-
-
-
+        }
+        
+        func getUpComingMovies() async {
+            let upComingResponse = await API.Movie.upComing.fetch(requestModel: [MovieResponse].self)
+            
             switch upComingResponse {
-            case let .success(response):
+            case .success(_):
                 break
             case let .failure(error):
                 print(error.localizedDescription)
